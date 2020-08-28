7342dab8cbbde98c0ad58000f5e29fca84cc4f6b
Duy Le Kim <lekimduy@hotmail.com>
2020-08-28 15:42:43 +0700

    Embed admin html

--- LkdCoreApp/Startup.cs
+++ LkdCoreApp/Startup.cs
@@ -112,6 +112,10 @@ namespace LkdCoreApp
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapRoute(
+                    name: "areaRoute",
+                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
